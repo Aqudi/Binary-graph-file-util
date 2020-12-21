14d49b7f89bfb9f12665b138cf61874242baefe7
Aqudi <gjdigj145@gmail.com>
2020-12-22 01:14:32 +0900

    feat: 실행결과 확인할 수 있도록 수정

--- test.cpp
+++ test.cpp
@@ -7,8 +7,15 @@ int main(int argc, char const *argv[]) {
   const char *in_path = "test.txt";
   const char *out_path = "test.bin";
 
-  cout << write_bin_file(in_path, out_path) << endl << endl;
-  cout << read_bin_file(out_path) << endl;
+  write_bin_file(in_path, out_path);
 
+  vector<vector<int>> adj = read_bin_file(out_path);
+  for (unsigned int i = 0; i < adj.size(); i++) {
+    cout << i << ": ";
+    for (unsigned int j = 0; j < adj[i].size(); j++) {
+      cout << adj[i][j] << " ";
+    }
+    cout << endl;
+  }
   return 0;
 }
