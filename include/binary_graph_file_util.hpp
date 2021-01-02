2e4fbf63f0c6f9973b2c8f416e8e226429037f73
Aqudi <gjdigj145@gmail.com>
2021-01-02 21:55:39 +0900

    feat: partition 함수 수정

--- include/binary_graph_file_util.hpp
+++ include/binary_graph_file_util.hpp
@@ -13,6 +13,7 @@
 #define uint unsigned int
 #define ull unsigned long long
 #define INT_SIZE SZ(uint)
+// #define DEBUG true
 
 /*
 전부 숫자인지 확인
@@ -77,12 +78,13 @@ void get_headers_from_bin(std::ifstream &in, uint &header_size,
   // Read header size
   in.read(reinterpret_cast<char *>(&header_size), INT_SIZE);
 
-  // Add header_size meta field
-  header_size += 1;
-
+  // Read headers
   uint *header_array = new uint[header_size];
   in.read(reinterpret_cast<char *>(&header_array[0]), INT_SIZE * header_size);
   headers.assign(header_array, header_array + header_size);
+
+  // Add header_size meta field
+  header_size += 1;
 }
 
 /*
@@ -105,14 +107,13 @@ std::vector<std::vector<int>> read_bin_file(const char *in_path) {
   vector<unsigned int> headers;
   get_headers_from_bin(in, header_size, headers);
 
-  for (uint i = 0; i < 10; i++) {
-    cout << "headers[" << i << "] :" << headers[i] << endl;
-  }
-
   int neighbor_size;
-  vector<vector<int>> adj(header_size);
-
   in.seekg(header_size * INT_SIZE, ios::beg);
+
+  // Remove total header size
+  header_size -= 1;
+
+  vector<vector<int>> adj(header_size);
   for (uint i = 0; i < header_size; i++) {
     in.read(reinterpret_cast<char *>(&neighbor_size), INT_SIZE);
     int *neighborhood = new int[neighbor_size];
@@ -131,19 +132,22 @@ std::vector<std::vector<int>> read_bin_file(const char *in_path) {
 /*
 Binary로 저장된 그래프의 일부를 읽는 함수
 */
-std::vector<int> read_bin_file_partition(std::ifstream &in, int header_size,
-                                         int body_pos) {
+std::vector<std::vector<int>> read_bin_file_partition(std::ifstream &in, int header_size,
+                                         int start, uint num_partition) {
   using namespace std;
 
-  int neighbor_size;
-  in.seekg(header_size * INT_SIZE + body_pos * INT_SIZE, ios::beg);
-  in.read(reinterpret_cast<char *>(&neighbor_size), INT_SIZE);
-  int *neighborhood = new int[neighbor_size];
-  in.read(reinterpret_cast<char *>(neighborhood), INT_SIZE * neighbor_size);
+  header_size += 1;
+  in.seekg(header_size * INT_SIZE + start * INT_SIZE, ios::beg);
 
-  vector<int> adj_list;
-  adj_list.assign(neighborhood, neighborhood + neighbor_size);
-  return adj_list;
+  vector<vector<int>> adj(num_partition);
+  int neighbor_size;
+  for(uint i=0; i<num_partition; i++) {
+    in.read(reinterpret_cast<char *>(&neighbor_size), INT_SIZE);
+    int *neighborhood = new int[neighbor_size];
+    in.read(reinterpret_cast<char *>(neighborhood), INT_SIZE * neighbor_size);
+    adj[i].assign(neighborhood, neighborhood + neighbor_size);
+  }
+  return adj;
 }
 
 /*
--- partition.cpp
+++ partition.cpp
@@ -2,13 +2,14 @@
 #include <fstream>
 #include <iostream>
 
+#define DEBUG true
 #define uint unsigned int
 
 using namespace std;
 
 int main(int argc, char const *argv[]) {
   const char *in_path = argv[1];
-  uint partition_size = atoi(argv[2]);
+  uint num_partition = atoi(argv[2]);
 
   // Read partition
   ifstream in;
@@ -22,18 +23,17 @@ int main(int argc, char const *argv[]) {
   vector<unsigned int> headers;
   get_headers_from_bin(in, header_size, headers);
 
-  for (uint i = 0; i < partition_size; i++) {
+  for (uint i = 0; i < num_partition; i++) {
     cout << "headers[" << i << "] :" << headers[i] << endl;
   }
 
-  vector<vector<int>> adj(partition_size);
-  for (uint i = 0; i < partition_size; i++) {
-    adj[i] = read_bin_file_partition(in, headers.size(), headers[i]);
-    cout << i << ": ";
-    for (uint j = 0; j < adj[i].size(); j++) {
-      cout << adj[i][j] << " ";
-    }
-    cout << endl;
+  vector<vector<int>> adj = read_bin_file_partition(in, headers.size(), 0, num_partition);
+  for (uint i = 0; i < adj.size(); i++) {
+  cout << i << ": ";
+  for (uint j = 0; j < adj[i].size(); j++) {
+    cout << adj[i][j] << " ";
+  }
+  cout << endl;
   }
 
   return 0;
